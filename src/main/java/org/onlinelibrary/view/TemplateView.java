4dd57f0c24ded851026baf6b87c9b716348fd5e1
Abylaziev Aman <abylgaziev_am@auca.kg>
2023-06-10 14:36:29 +0600

    add README

--- src/main/java/org/onlinelibrary/view/TemplateView.java
+++ src/main/java/org/onlinelibrary/view/TemplateView.java
@@ -5,11 +5,9 @@ import org.springframework.ui.Model;
 
 @Slf4j
 public abstract class TemplateView {
-
   public String loadView(Model model, String view) {
     log.info("Directing to " + view);
     model.addAttribute("content", view);
     return "template";
   }
-
 }
